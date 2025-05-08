2977d15421ddde2848403cbb872fd15c1920ec64
cho-seunghee <nurcle77@gmail.com>
2025-05-08 17:01:12 +0900

    Updates

---
+++ assets/test1-Bbedm9PZ.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-B8xKNlWv.js";const o=()=>(console.log("Test1Page: Component Rendered"),e.jsx("div",{children:"Test1 Page"}));export{o as default};
+//# sourceMappingURL=test1-Bbedm9PZ.js.map
---
+++ assets/test2-DpcEW2WC.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-B8xKNlWv.js";const s=()=>t.jsx("div",{children:"Test2 Page"});export{s as default};
+//# sourceMappingURL=test2-DpcEW2WC.js.map
