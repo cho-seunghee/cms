ba04017066d86a8fed7e5d8581655b2a04f04daa
cho-seunghee <nurcle77@gmail.com>
2025-05-13 17:23:12 +0900

    Updates

--- assets/test2-D4NUMQJ6.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-DKCjJ4Gs.js";const s=()=>t.jsx("div",{children:"Test2 Page"});export{s as default};
-//# sourceMappingURL=test2-D4NUMQJ6.js.map
---
+++ assets/test2-DGJWsH1l.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-CjkxrXBI.js";const s=()=>t.jsx("div",{children:"Test2 Page"});export{s as default};
+//# sourceMappingURL=test2-DGJWsH1l.js.map
