eb9a714ce80dd9fd9e97749834d2bd9f2c7a40ea
cho-seunghee <nurcle77@gmail.com>
2025-05-25 14:36:10 +0900

    Updates

---
+++ assets/test1-BrXE_vO9.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-BdnnkKSA.js";const o=()=>(console.log("Test1Page: Component Rendered"),e.jsx("div",{children:"Test1 Page"}));export{o as default};
+//# sourceMappingURL=test1-BrXE_vO9.js.map
---
+++ assets/test2-DPJV4iPg.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-BdnnkKSA.js";const s=()=>t.jsx("div",{children:"Test2 Page"});export{s as default};
+//# sourceMappingURL=test2-DPJV4iPg.js.map
